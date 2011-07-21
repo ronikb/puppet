c7b164948686069886d037c347ed7e0948b18eb9
roni <ronykb@yahoo.com>
2011-07-21 10:28:10 +0000

    removed $1 from db-restore

--- scripts/db-restore.sh
+++ scripts/db-restore.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
-mysql_password=$1
-newdb=$2
-db_destination_for_restore=$3
+mysql_password=
+newdb=$1
+db_destination_for_restore=$2
 mysql --user=root --password=$mysql_password  $newdb < $db_destination_for_restore
 echo "Database restored"
 
