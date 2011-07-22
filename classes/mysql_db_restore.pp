8da9004ad6d823790e3a59af9a4c2d7ada5d1c96
Roni <roni@citrusinformatics.com>
2011-07-22 14:37:58 +0530

    edit mysql db restore file name

--- classes/mysql_db_restore.pp
+++ classes/mysql_db_restore.pp
@@ -1,7 +1,7 @@
 # This will create MySql database 
 	class db-restore {
         exec { "db-restore":
-                command =>"/etc/puppet/manifests/scripts/db-restore.sh $mysql_dbname $mysql_dump_location_for_dbrestore",
+                command =>"/etc/puppet/manifests/scripts/mysql-db-restore.sh $mysql_dbname $mysql_dump_location_for_dbrestore",
                 require => Exec['db-create'],
 }
 }
