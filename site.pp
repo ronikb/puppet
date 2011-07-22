c02c7d6391e68a431b5d08396ed72f321f08c922
Roni <roni@citrusinformatics.com>
2011-07-22 14:12:23 +0530

    updated site.pp file with comments

--- site.pp
+++ site.pp
@@ -1,3 +1,4 @@
+# These all are variables. We can edit this (Double coated value)for customisation
 $gitclone_application="git@github.com:netspective/medigy-drupal.git"
 $gitclone_application_destination="/var/www/html/edge.devl.medigy.com/medigy-drupal/"
 $gitclone_db="git@github.com:netspective/medigy-drupal-db.git"
@@ -10,10 +11,15 @@ $mysql_dbname="medigy"
 $symlink_files_folder_source="/medigy/medigy-drupal-db/files"
 $symlink_files_folder_destination="/var/www/html/edge.devl.medigy.com/medigy-drupal/public_site/sites/default/files"
 
+#This will import all manifests under this 'classes' folder.
 import "classes/*"
+
 Exec { 
     path => ["/bin", "/sbin", "/usr/bin", "/usr/sbin"], 
 }
+
+# If you want to run this manifest for a specified system, put the system name instead of default. Default means it will run all the systems.
+# If you want to disable any of the following installation or configuration changes, Please do delete that line and execute this manifest.
 node default {
 	include apache
 	include php
