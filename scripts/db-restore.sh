7bd4210bc20041a5cfa07e9ddcfac7851f73f2af
Roni <roni@citrusinformatics.com>
2011-07-22 12:27:27 +0530

    made changes for $mysql_dump_location_for_dbrestore

--- scripts/db-restore.sh
+++ scripts/db-restore.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 mysql_password=
 mysql_dbname=$1
-db_destination_for_restore=$2
-mysql --user=root --password=$mysql_password  $mysql_dbname < $db_destination_for_restore
+mysql_dump_location_for_dbrestore=$2
+mysql --user=root --password=$mysql_password  $mysql_dbname < $mysql_dump_location_for_dbrestore
 echo "Database restored"
 
